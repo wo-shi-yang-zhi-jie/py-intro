c3fa3afb499beda0e5c2136dcdbc60ee7a05f332
wo-shi-yang-zhi-jie <98301661+wo-shi-yang-zhi-jie@users.noreply.github.com>
2022-01-25 10:41:55 +0800

    第一堂課

--- 圓.py
+++ 圓.py
@@ -7,5 +7,5 @@ This is a temporary script file.
 
 r = input("半徑是多少? 半徑是:")
 print("圓周率是3.14")
-print("圓周長是",int(r)*2*3.14)#算圓積
-print("圓面積是",int(r)**2*3.14)#算圓積
+print("圓周長是",float(r)*2*3.14)#算圓周長
+print("圓面積是",float(r)**2*3.14)#算圓面積
