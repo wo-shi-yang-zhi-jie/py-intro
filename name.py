9e9e50cd66ebe7d68d83e5f4b8c6f51031f0d9b5
wo-shi-yang-zhi-jie <98301661+wo-shi-yang-zhi-jie@users.noreply.github.com>
2022-01-25 10:17:40 +0800

    第一堂課
    name是say hi
    圓算圓面積

---
+++ name.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 24 11:34:29 2022
+
+@author: yishe
+"""
+name = input("What's your name?  your name is : ")
+#print(name)
+print("hello", name)
+print("You're talking to a computer!")
+mood = input("What's your mood?  your mood is : ")
+#輸入你的心情
+print("I'm so ",mood,",too!")
---
+++ 圓.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+r = input("半徑是多少? 半徑是:")
+print("圓周率是3.14")
+print("圓面積是",int(r)**2*3.14)#算圓積
